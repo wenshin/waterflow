5d76ff9d9d4470f17da6d0e801bbd9249ca08a9d
yuanwen <wenshin2011@gmail.com>
2016-01-26 09:27:31 +0800

    调整结构

--- test/pipeline.js
+++ test/pipeline.js
@@ -1,35 +1,118 @@
 import {assert} from 'chai';
-import Pipeline from '../lib';
-import PipelineLogger from '../lib/middlewares/logger';
+import pipeline from '../lib/pipeline';
+import PipelineLoggerMiddleware from '../lib/middlewares/logger';
+import {makeRoundNumberHandler, toNumberHandler} from '../lib/middlewares/number';
 
-describe('Pipeline', function () {
-  it('应该正确运行委托自 Array 的方法', function () {
-    let pipeline = new Pipeline(
-      'Pipeline Array test',
-      [
-        {name: 'pipe1', handle: v => v},
-        {name: 'Negative', handle: v => -v}
-      ]
-    );
+let RoundNumberPipelineMiddleware = {
+  type: 'pipeline',
+  name: 'RoundNumberPipelineMiddleware',
+  post: makeRoundNumberHandler()
+};
 
-    assert.equal(pipeline.flow(10), -10);
+let RoundNumberPipeMiddleware = {
+  type: 'pipe',
+  name: 'RoundNumberPipeMiddleware',
+  post: makeRoundNumberHandler()
+};
 
-    pipeline.push({name: 'MultiplicativeInverse', handle: v => 1 / v});
+let ToNumberPipelineMiddleware = {
+  type: 'pipeline',
+  name: 'ToNumberPipelineMiddleware',
+  pre: toNumberHandler
+};
 
-    assert.equal(pipeline.flow(10), -0.1);
+
+describe('pipeline', function () {
+  it('应该正确运行非异步方法', function () {
+    let except = pipeline('10',
+      {
+        name: 'pipeline1',
+        middlewares: [ToNumberPipelineMiddleware]
+      })
+      .flow(v => {
+        if (typeof v !== 'number') throw new TypeError('not a number');
+        return v;
+      })
+      .flow(v => -v)
+      .flow(v => 1/v)
+      .finish();
+    assert.equal(except, -0.1);
   });
 
-  it('应该正确运行 Logger 中间件', function () {
-    let pipeline = new Pipeline(
-      'Pipeline Logger test',
-      [
-        {name: 'pipe1', handle: v => v},
-        {name: 'Negative', handle: v => -v}
-      ]
-    );
-    Pipeline.pipelineMiddlewares.push(PipelineLogger);
-    assert.equal(pipeline.flow(10), -10);
-    Pipeline.pipelineMiddlewares = [];
+  it('应该正确运行异步方法', function (done) {
+    let asyncPipe = data => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(data * 2);
+        }, 10);
+      });
+    };
+    let promise = pipeline(10,
+      {
+        name: 'pipeline2',
+        middlewares: [RoundNumberPipelineMiddleware]
+        // middlewares: [RoundNumberPipelineMiddleware, PipelineLoggerMiddleware]
+      })
+      .flow(v => -v)
+      .flowAsync(asyncPipe)
+      .flow(v => 1/v)
+      .flowAsync(asyncPipe)
+      .flow(v => v * 3)
+      .finish();
+    assert.ok(promise instanceof Promise, '异步结果返回 Promise 对象');
+
+    let except;
+    promise
+      .then(data => {
+        except = data;
+      });
+    setTimeout(() => {
+      assert.equal(except, -0.3);
+      done();
+    }, 30);
+  });
+
+  it('可以正确用 flowMap 处理对象', function () {
+    let except = pipeline({1: 10, 2: 20, 3: 30})
+    // let except = pipeline({1: 10, 2: 20, 3: 30}, {middlewares: [PipelineLoggerMiddleware]})
+      .flowMap({handle: v => 1/v, filter: v => v < 30})
+      .flowReduce({
+        handle: (pre, cur) => pre + cur,
+        initialValue: 0,
+        middlewares: [RoundNumberPipeMiddleware]
+      })
+      .finish();
+    assert.equal(except, 0.15);
+  });
+
+  // it('应该正确处理异步管道出错', function (done) {});
+
+  it('可以正确执行同步方法 flowMap 和 flowReduce', function () {
+    let except = pipeline(10)
+    // let except = pipeline(10, {middlewares: [PipelineLoggerMiddleware]})
+      .flow(v => [1*v, 2*v, 3*v])
+      .flowMap({handle: v => 1/v, filter: v => v < 30})
+      .flowReduce({
+        handle: (pre, cur) => pre + cur,
+        initialValue: 0,
+        middlewares: [RoundNumberPipeMiddleware]
+      })
+      .finish();
+    assert.equal(except, 0.15);
+  });
+
+  it('可以使用 handles 传递多个处理函数执行 flowMap ', function () {
+    let except = pipeline(10)
+    // let except = pipeline(10, {middlewares: [PipelineLoggerMiddleware]})
+      .flow(v => [1*v, 2*v, 3*v])
+      .flowMap({handles: [v => 1 / v, v => 1 / v + 1, v => 1 / v + 2], filter: v => v < 30})
+      .flowReduce({
+        handle: (pre, cur) => pre + cur,
+        initialValue: 0,
+        middlewares: [RoundNumberPipeMiddleware]
+      })
+      .finish();
+    assert.equal(except, 1.15);
   });
 
 });
